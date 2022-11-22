da2b1dbf7584a4a50b5b6e0905ec40c2fb6cb6b3
kanumarlapudi <180020034ce@gmail.com>
2022-11-22 10:52:35 +0530

    First commit

---
+++ JAVA_BASIC_PROGRAMS/src/RandomNumbers.java
@@ -0,0 +1,13 @@
+import java.lang.Math;           // we have to import math class
+public class RandomNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// we will get only double values by using this method for int there is another way
+        System.out.println("First random number is " + Math.random()); //  Math.random() is used to get random numbers
+        System.out.println("Second random number is " + Math.random());
+        
+	}
+
+}
