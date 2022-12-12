df9f0e961e174a59cc16b7d4894dd077aa9424ca
kanumarlapudi <180020034ce@gmail.com>
2022-12-12 12:48:27 +0530

    First commit

--- JAVA_BASIC_PROGRAMS/src/RandomNumbers.java
+++ JAVA_BASIC_PROGRAMS/src/RandomNumbers.java
@@ -8,6 +8,12 @@ public class RandomNumbers {
         System.out.println("First random number is " + Math.random()); //  Math.random() is used to get random numbers
         System.out.println("Second random number is " + Math.random());
         
+        int max = 400;
+        int min = 200;
+        int random = (int) (Math.random() * (max - min + 1) + min);  //to get value between 200 to 400;
+        System.out.println(random);
+        
+        
 	}
 
 }
